d27ef844aa03a1109f1df00cd7c70141ec7d9464
fatemehragh <fateme.ragh@gmail.com>
2019-09-02 01:07:51 +0430

    masterr

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
+
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1000);
+}
+
+(function shuffle(){
+    cards.forEach(card=>{
+        let randomPos = Math.floor(Math
+            .random()*12);
+        card.style.order = randomPos
+    })
+})()
+
+cards.forEach(card => card.addEventListener('click', flipCard));
